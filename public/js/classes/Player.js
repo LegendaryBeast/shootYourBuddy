6f72dfe32849f61ba29f91f2657e2aadfa50264e
Abdullah Al Mahmud <138648851+2021331012abdullah@users.noreply.github.com>
2023-12-28 12:15:23 +0600

    Add files via upload

--- public/js/classes/Player.js
+++ public/js/classes/Player.js
@@ -1,24 +1,31 @@
 class Player {
-  constructor({ x, y, radius, color, username }) {
+  constructor({ x, y, imageURL, color, username, radius }) {
     this.x = x
     this.y = y
-    this.radius = radius
+    this.imageURL = imageURL
     this.color = color
     this.username = username
+    this.radius = radius
+    this.image = new Image(radius*2, radius*2)
+    this.image.src = urlPrefix + this.imageURL
   }
+
 
   draw() {
 
-    c.font = '12px sans-serif'
+    c.font = '16px sans-serif'
     c.fillStyle = 'white'
-    c.fillText(this.username, this.x - 10, this.y + 40)
+    c.fillText(this.username, this.x - this.radius, this.y + this.radius + 20)
     c.save()
     c.shadowColor = this.color
     c.shadowBlur = 20
     c.beginPath()
-    c.arc(this.x, this.y, this.radius * 2, 0, Math.PI * 2, false)
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+    // c.closePath()
     c.fillStyle = this.color
     c.fill()
+    c.clip()
+    c.drawImage(this.image, this.x-this.radius, this.y-this.radius, this.radius*2, this.radius*2);
     c.restore()
   }
 }
